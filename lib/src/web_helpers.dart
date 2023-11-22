d509d1b87f98ebfff8f3adac1533d44ee4c06778
Kevin Moore <kevmoo@google.com>
2023-11-22 10:39:12 -0800

    nit

--- lib/src/web_helpers.dart
+++ lib/src/web_helpers.dart
@@ -1,5 +1,7 @@
 import 'package:web/helpers.dart';
 
+// TODO: remove when https://github.com/dart-lang/web/pull/102 is landed
+// and the min constraint on pkg:web is updated
 extension WebSocketEvents on WebSocket {
   Stream<Event> get onOpen => EventStreamProviders.openEvent.forTarget(this);
   Stream<MessageEvent> get onMessage =>
