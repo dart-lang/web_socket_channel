43ce1973d066d8ffa05524fd2a943631e1e4056f
Kevin Moore <kevmoo@google.com>
2023-11-22 10:35:21 -0800

    Use EventStreamProviders

--- lib/src/web_helpers.dart
+++ lib/src/web_helpers.dart
@@ -1,12 +1,11 @@
 import 'package:web/helpers.dart';
 
-extension WebSocketExtension on WebSocket {
-  Stream<Event> get onOpen =>
-      const EventStreamProvider<Event>('open').forTarget(this);
+extension WebSocketEvents on WebSocket {
+  Stream<Event> get onOpen => EventStreamProviders.openEvent.forTarget(this);
   Stream<MessageEvent> get onMessage =>
-      const EventStreamProvider<MessageEvent>('message').forTarget(this);
+      EventStreamProviders.messageEvent.forTarget(this);
   Stream<CloseEvent> get onClose =>
-      const EventStreamProvider<CloseEvent>('close').forTarget(this);
+      EventStreamProviders.closeEvent.forTarget(this);
   Stream<Event> get onError =>
-      const EventStreamProvider<Event>('error').forTarget(this);
+      EventStreamProviders.errorEventSourceEvent.forTarget(this);
 }
